4cbba47cebd9e27ae846c0c2150196d194ceeaf8
aakashDeveloper <ahanda205@gmail.com>
2018-11-24 17:11:22 +0000

    a

---
+++ first.py
@@ -0,0 +1,66 @@
+print("hii to python")
+###Java
+int
+double
+float 
+String
+
+#####Python
+String=> any thing in "" "sri" "10" '10' "true"
+Number => 1, 34, 987989, 998776, 67, 677 1.1 43878.48
+Boolean=> True or False
+
+name = "Python"
+marks = 20
+result = False
+
+
+>>> result = False
+>>> result
+False
+>>> random = '1123dfdg'
+>>> pi
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'pi' is not defined
+>>> pi = 3.145
+>>> marks + pi
+13.145
+>>> name+ marks
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: cannot concatenate 'str' and 'int' objects
+>>> marks + name
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+>>> name + random
+'python1123dfdg'
+>>> name+str(marks)
+'python10'
+>>> "python"+10
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: cannot concatenate 'str' and 'int' objects
+
+
+= is assignment
+== is comparison
+ 'hi' == "hi"
+True
+>>> 'hi' == """hi"""
+
+isdigit
+if input is number inside "" =  True
+if input is string inside "" =  False
+if input is combination of number & string = False
+
+to convert  string to number use => int
+to convert  number to string use => str
+
+['A', 'B', 'C']
+[1,2,3,45,6]
+
+Array in python is collection of homegenious data type
+['A', 1 ,3, 'B', True]
+
